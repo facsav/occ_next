af51bc504ef7d1bcc542d815b74a2f167f02d6d8
facSav <facu.savio@gmail.com>
2024-10-02 08:20:47 -0300

    a

--- src/components/carrusel.jsx
+++ src/components/carrusel.jsx
@@ -16,6 +16,7 @@ export const Carrusel = () => {
   useEffect(() => {
     const scrollers = document.querySelectorAll(".scroller");
 
+    
     // If a user hasn't opted in for recuded motion, then we add the animation
     if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
       addAnimation();
