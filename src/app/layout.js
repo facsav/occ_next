adab9c7522022b949445815ec26ffdb1106b3c6d
facSav <facu.savio@gmail.com>
2024-10-27 18:33:50 -0300

    arreglo home css

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import Footer from '@/components/Footer';
 import BootstrapClient from '@/components/BoostrapClient';
 import { Inter } from 'next/font/google';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
-import '../styles/App.css';
+import  '../styles/Home.css';
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Home from "@/components/Home";
+import style from '../styles/App.css';
 
 
 export default function Page() {
